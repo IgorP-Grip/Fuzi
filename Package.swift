16b921cf7af377ab15a3b5b6d06567b745ad6929
Igor Petrovic <igor.petrovic@intros.at>
2021-12-21 09:59:04 +0100

    Package is using XCFramework created from master branch for targets arm64 and x86_64

--- Package.swift
+++ Package.swift
@@ -1,21 +1,17 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Fuzi",
+	platforms: [
+        .iOS(.v12)
+	],
     products: [
-        .library(name: "Fuzi", targets: ["Fuzi"]),
+        .library(name: "Fuzi", targets: ["Fuzi"])
     ],
     targets: [
-        .target(name: "Fuzi",
-            path: "Sources",
-            linkerSettings: [.linkedLibrary("xml2")]
-        ),
-        .testTarget(name: "FuziTests",
-                    dependencies: ["Fuzi"],
-                    path: "Tests"
-        )
+        .binaryTarget(name: "Fuzi", path: "Sources/Fuzi/Fuzi.xcframework")
     ]
 )
